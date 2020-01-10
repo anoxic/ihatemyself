cdaa1fa66ee0a1d256790cf6855c3d1c90a3dd06
Brian Zick <brian@zick.io>
2020-01-10 00:27:47 -0500

    comment list: same css, rem html for now

--- comment_list.php
+++ comment_list.php
@@ -6,41 +6,32 @@ $bucket = bucket(substr($_SERVER{'REQUEST_URI'}, strlen('/comments/')));
 $comments = r()->lrange("comments:$bucket", 0, 99);
 $prefix = comment_path();
 ?>
-<title>comments</title>
-<style>
-a           { color: #710f26 }
-tag,date,sy { color: #b1a3a3 }
-co          { color: #557ec1 }
-*           { color: #433838; font: 1em/3.2ex monospace; padding: 0 }
-body        { margin: 3ex 3ex 7ex; width: 77ex }
-em,author   { font-weight: bold }
-author      { display: block; margin: 0 0 1ex }
-[comment]   { margin: 3ex 0 0 }
-[comment]:last-child { margin-bottom: 5ex }
-</style>
-<h1><em>Comments</em> <sy><?=$bucket?></sy></h1>
-<div>
+<link rel=stylesheet href=/cee_ess_ess>
+<title>Comments</title> <sy><?=$bucket?></sy>
+<div wrap>
 <?php
 if (!$comments) {
     echo "<p><co>/* listing nil comments. the nothingness has overtaken us. where's atreyu when ya need 'im */</co></p>";
 }
 foreach ($comments as &$c) {
     $c = json_decode($c, 1);
-    if (strpos($c['website'], '//') === false) {
+    if ($c['website'] && strpos($c['website'], '//') === false) {
         $c['website'] = 'http://' . $c['website'];
     }
     //XXX: need to filter website plus any href values
-    $c['website'] = strtr($c['website'], '"', '');
+    $c['website'] = $c['website'] ? strtr($c['website'], '"', '') : null;
     $c['name']    = htmlspecialchars($c['name']);
-    $c['comment'] = preg_replace('#<(/)?(a( href="[^>\]]+")?|em|strong|code)?>#', '\\[$1$2\\]', $c['comment']);
     $c['comment'] = nl2br(htmlspecialchars($c['comment']));
+    /*
+    //XXX: actually filter [ inputs
+    $c['comment'] = preg_replace('#<(/)?(a( href="[^>\]]+")?|em|strong|code)?>#', '\\[$1$2\\]', $c['comment']);
     $c['comment'] = preg_replace('#a href=([^\]]+)#', 'a rel="noopener noreferrer" target=_blank href=$1', $c['comment']);
     $c['comment'] = strtr($c['comment'], ['\[' => '<', '\]' => '>']) ;
-    //XXX: actually filter [ inputs
+     */
     $auth = $c['website']
-        ? "$c[name] (<a rel=\"noopener noreferrer\" target=_blank href=\"$c[website]\">site</a>)"
+        ? "<a rel=\"noopener noreferrer\" target=_blank href=\"$c[website]\" title=website>@</a> $c[name]"
         : "$c[name]";
-    echo "<div comment><author>$auth <date>$c[date]</date></author> \n$c[comment]</div>";
+    echo "<div comment><author>$auth <time>$c[date]</time></author> \n$c[comment]</div>";
 }
 ?>
 </div>
