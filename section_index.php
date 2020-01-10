48e75ccef14df81d5fbe85d93a0fbebb55e404cb
Brian Zick <brian@zick.io>
2020-01-09 22:59:08 -0500

    correct path

--- section_index.php
+++ section_index.php
@@ -2,7 +2,7 @@
 <title>posts from <?=$d=substr($_SERVER{'REQUEST_URI'},1,4)?></title>
 <pre>
 <?php
-include dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+include dirname(dirname(dirname(__FILE__))) . '/engine/vendor/autoload.php';
 echo "<a href=..>..</a>\n";
 foreach(json_decode(r()->get('post_index')?:'[]', 1) as $p) {
     foreach (glob("*/index.php") as $f) {
